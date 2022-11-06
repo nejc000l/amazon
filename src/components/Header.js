6a86f8619489760d7f2333a8fe26d9e73cb11ad1
nejc000l <nejc.lampic@gmail.com>
2022-11-06 12:42:58 +0100

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import{MagnifyingGlassIcon
+import{MagnifyingGlassIcon,ShoppingCartIcon,
 } from '@heroicons/react/24/outline'
 function Header() {
   return (
@@ -19,9 +19,22 @@ function Header() {
           <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focuse:outline-none px-4" type="text" />
           <MagnifyingGlassIcon className="h-12 p-4" />
           </div>
-      </div>
-      <div>
+      <div className="text-white flex items-center text-xs mx-6 space-x-6 whitespace-nowrap"> 
+          <div className="link">
+            <p>Hello Nejc L</p>
+            <p>Account & Lists</p>
+          </div>
+          <div className="link">
+          <p>Returns</p>
+          <p>& Orders</p>
 
+          </div>
+          <div className="link">
+            <ShoppingCartIcon className="h-10"/> 
+            <p>Basket</p>
+            
+          </div>
+      </div>
       </div>
     </header>
   );
