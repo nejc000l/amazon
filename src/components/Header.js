087fdd0efdbe09fbd7ad44890a7eb4fef07becb5
nejc000l <nejc.lampic@gmail.com>
2022-11-08 18:19:56 +0100

    products and main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import{MagnifyingGlassIcon,ShoppingCartIcon,
-} from '@heroicons/react/24/outline'
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+
+
 function Header() {
   return (
     <header>
@@ -16,25 +21,46 @@ function Header() {
           />
         </div>
         <div className=" hidden sm:flex rounded-md flex-grow cursor-pointer items-center h-10 hover:bg-yellow-500 bg-yellow-400">
-          <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focuse:outline-none px-4" type="text" />
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focuse:outline-none px-4"
+            type="text"
+          />
           <MagnifyingGlassIcon className="h-12 p-4" />
-          </div>
-      <div className="text-white flex items-center text-xs mx-6 space-x-6 whitespace-nowrap"> 
+        </div>
+        <div className="text-white flex items-center text-xs mx-6 space-x-6 whitespace-nowrap">
           <div className="link">
             <p>Hello Nejc L</p>
-            <p>Account & Lists</p>
+            <p className="font-extrabold md:text-sm ">Account & Lists</p>
           </div>
           <div className="link">
-          <p>Returns</p>
-          <p>& Orders</p>
-
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm ">& Orders</p>
           </div>
-          <div className="link">
-            <ShoppingCartIcon className="h-10"/> 
-            <p>Basket</p>
-            
+          <div className="link relative flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-extrabold md:text-sm hidden md:inline mt-2 ">
+              Basket
+            </p>
           </div>
+        </div>
       </div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <Bars3Icon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
